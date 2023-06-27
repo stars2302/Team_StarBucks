da9183d248dbfae19a198f66317294578bf3c9c8
stars2302 <stars2302@naver.com>
2023-06-27 18:04:37 +0900

    [Add] login submit feedback js 코드 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
-let login_btn = document.querySelector('.modal_wrap .submit');
+// let login_btn = document.querySelector('.modal_wrap .submit');
 
-login_btn.addEventListener('click',(e)=>{
+// login_btn.addEventListener('click',(e)=>{
+//   e.preventDefault();
+// });
+
+let modal_loginForm = document.querySelector('header .modal_wrap .login_form');
+let modal_loginInput = modal_loginForm.querySelectorAll('.field input');
+let modal_loginFeedback = modal_loginForm.querySelector('.login_func .feedback');
+
+//로그인 폼에 전송이벤트가 일어나면
+modal_loginForm.addEventListener('submit',(e)=>{
+  //전송기능을 막고
   e.preventDefault();
+  console.log(e);
+
+
+  let completed = 0;
+  //전송실패 시
+  for(let ip of modal_loginInput){
+    //인풋에 값이 없으면
+    if(!ip.value){
+
+      //값이 없는 인풋에 포커스를 주고
+      ip.focus();
+
+      //피드백문구를 띄운다.
+      modal_loginFeedback.style.display = 'block';
+
+      return false;
+      
+      //모두 값이 있으면 completed를 1씩 올려서
+    } else{
+      ++completed
+      console.log(completed);
+    }
+  }
+
+  //전송성공 시
+  if(completed == modal_loginInput.length){
+    let text = '<h2 class="sub_tt title_pd font_gray">로그인에 성공했습니다!</h2>';
+    modal_loginForm.querySelector('.inner_form').innerHTML = text;
+    modal_loginForm.querySelector('h2').style.fontSize = '30px'
+    modal_loginForm.querySelector('h2').style.lineHeight = '240px'
+  }
 });
