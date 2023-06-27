420a176460f5fd562042e9f1f6ece0edd2f93aaf
stars2302 <stars2302@naver.com>
2023-06-27 15:54:44 +0900

    [Add] javascript 주석 설명 추가

--- js/main.js
+++ js/main.js
@@ -3,41 +3,29 @@ let sec1_bigItem = document.querySelector('.sec1 .big_item');
 let sec1_bigItemImg = sec1_bigItem.querySelector('img');
 
 
+//작은이미지를 클릭하면
 sec1_smallItem.forEach((img,idx)=>{
   img.addEventListener('click',()=>{
     let targetIdx = idx+1;
-    
-    debounce(changeImg(sec1_bigItemImg,targetIdx),800);
-    ;
+    //이미지 바꾸는 함수 실행
+    changeImg(sec1_bigItemImg,targetIdx);
   });//click
 }); 
 
 //모션주며 이미지 바꾸기
 function changeImg(target,index){
+  //일단 이미지 숨기고
   target.classList.add('hidden');
-  //
+  
+  //잠깐의 시간 후 이미지경로 바뀌면서 나타나면서 크기키우기
   setTimeout(()=>{
     target.setAttribute('src',`img/sec1_item${index}_big.png`);
     target.classList.remove('hidden');
     target.classList.add('active');
   },10);
   
+  //크기 키우는 모션이 끝나면 ACTIVE 지우기
   setTimeout(()=>{
     target.classList.remove('active');
   },800);
-}
-
-
-//모션이 완료되기 전에 클릭하지 못하도록 막기
-function debounce(callback, time){
-  let slideTrigger = true;
-  return ()=>{
-    if(slideTrigger){
-      callback();
-      slideTrigger = false;
-      setTimeout(()=>{
-        slideTrigger= true;
-      },time)
-    }
-  }
 }
