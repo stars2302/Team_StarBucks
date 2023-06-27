fa86a09e287e859fb9dbfcb518d8fd2fcac7f349
sunchoi22 <103012829+sunchoi22@users.noreply.github.com>
2023-06-27 15:53:43 +0900

    [Chore] sec2.js 수정
     sec2.js 수정했습니다.

--- js/sec2.js
+++ js/sec2.js
@@ -1,24 +1,16 @@
 let slideWrapper = document.querySelector('.slide_wrapper'),
     slideContainer = document.querySelector('.slides'),
-    slides = slideContainer.querySelectorAll('.ice'),
-    tables = slideContainer.querySelectorAll('.sce2_table'),
-    icetexts = slideContainer.querySelectorAll('.icetext'),
+    slides = slideContainer.querySelectorAll('.slides li'),
     slideCount = slides.length,
     slidePerView = 3,
     slideWith = 360,
     slideMargin = 30,
     currentSlideIdx = 0;
-    prevBtn = document.querySelector('.prev'),
-    nextBtn = document.querySelector('.next');
+    prevBtn = document.querySelector('.sec2prev'),
+    nextBtn = document.querySelector('.sec2next');
 
     slides.forEach((slide,idx)=>{
       slide.style.left = `${idx*(slideWith+slideMargin)}px`
-    })
-    tables.forEach((table,idx)=>{
-      table.style.left = `${idx*(slideWith+slideMargin)}px`
-    })
-    icetexts.forEach((icetext,idx)=>{
-      icetext.style.left = `${idx*(slideWith+slideMargin)}px`
     })
 
     //슬라이드 이동함수
@@ -29,13 +21,8 @@ let slideWrapper = document.querySelector('.slide_wrapper'),
 
     //버튼 클릭하면 움직이기
     nextBtn.addEventListener('click',()=>{
-      if(currentSlideIdx < slideCount -slidePerView){
-        moveSlide(currentSlideIdx + 3);
-      }
-    });
-    nextBtn.addEventListener('click',()=>{
       if(currentSlideIdx < slideCount-slidePerView){
-        moveSlide(currentSlideIdx + 1);
+        moveSlide(currentSlideIdx + 3);
       }
     });
 
