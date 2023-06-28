50d86e9fd9196f97f544676eed46e9ad9836f098
stars2302 <stars2302@naver.com>
2023-06-28 16:03:58 +0900

    merge

--- js/main.js
+++ js/main.js
@@ -147,3 +147,22 @@ let slideWrapper = document.querySelector('.sec2slide_wrap'),
       }
     });
 /* sec2 bestSlide_최성희(종료) */
+
+/* sec3 video_나서영(시작) */
+let sec3_banner = document.querySelector('.sec3 .container');
+let sec3_modal = document.querySelector('.sec3 .modal_wrap');
+
+
+//sec3 banner클릭하면 modal의 visible클래스를 추가하여 나타나기
+sec3_banner.addEventListener('click',(e)=>{
+  e.preventDefault();
+  sec3_modal.classList.add('visible');
+});
+
+
+//modal 아무곳이나 클릭하면 visible클래스를 제거하여 숨기기
+sec3_modal.addEventListener('click',(e)=>{
+  e.preventDefault();
+  sec3_modal.classList.remove('visible');
+});
+/* sec3 video_나서영(종료) */
