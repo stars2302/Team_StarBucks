3a8d31900ef1837d8924a1266a80b040c2a9e917
teastz <137160482+teastz@users.noreply.github.com>
2023-06-29 00:57:34 +0900

    [Add] html 정리 / css 선택자, 패딩,마진 정리 / js 변경
    js스크립트 수정에 의한 css 전체적인 수정보완

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-let headerz = document.querySelector('.bottom_menu'),
+/* header_성진영(시작) */
+    
+let headerz = document.querySelector('header'),
+body = document.querySelector('body'),
+botmenu = document.querySelector('.bottom_menu'),
 menu = document.querySelectorAll('.bottom_menu .menu_list li'),
 submenu = document.querySelectorAll('.bottom_menu .menu_list dl'),
-headerHeight = headerz.offsetHeight;
+header_Topsize = document.querySelector('.top_menu'),
+headerTop = header_Topsize.offsetHeight,
+headerHeight = headerz.offsetHeight; 
 let submenuHeight = 0;
+
 
 submenu.forEach(item=>{
 if(item.offsetHeight > submenuHeight){
@@ -14,17 +21,30 @@ let headerzTotal = `${headerHeight + submenuHeight}px`;
 
 menu.forEach(tem=>{
 tem.addEventListener('mouseover',()=>{
-    headerz.style.height = headerzTotal;
+    if(window.scrollY == 0){
+        headerz.style.height = headerzTotal;
+    }else{
+        headerz.style.height = `${botmenu.offsetHeight + submenuHeight}px`;
+    }
 });
 tem.addEventListener('mouseout',()=>{
-    headerz.style.height = `${headerHeight}px`;
+    if(window.scrollY == 0){
+        headerz.style.height = `${headerHeight}px`;
+    }else{
+        headerz.style.height = `${botmenu.offsetHeight}px`;
+    }
 });	
 });
 
 window.addEventListener('scroll',()=>{
-if(window.scrollY > headerHeight){
-    headerz.classList.add('sticky');
+if(window.scrollY > headerTop){
+    body.classList.add('sticky');
+    headerz.style.height = `${botmenu.offsetHeight}px`;
 }else{
-    headerz.classList.remove('sticky');
+    body.classList.remove('sticky');
+    headerz.style.height = `${headerHeight}px`;
+    
 }
 });
+
+/* header_성진영(종료) */
