e713f41e351f897f92f0f3679ed4b700ecbcde92
stars2302 <stars2302@naver.com>
2023-06-28 16:13:28 +0900

    merge

--- js/main.js
+++ js/main.js
@@ -166,3 +166,28 @@ sec3_modal.addEventListener('click',(e)=>{
   sec3_modal.classList.remove('visible');
 });
 /* sec3 video_나서영(종료) */
+
+/* sec4 promotion_천혜영(시작) */
+/* sec4 promotion_천혜영(종료) */
+
+/* sec5 store_최성희(시작) */  
+let targetLink =document.querySelectorAll('.sec5 .tab_menu a');
+let tabContent = document.querySelectorAll('.sec5 #tab_content > div');
+//a태그들마다 클릭하면 할 일
+for(let i = 0; i <targetLink.length; i++){
+
+  targetLink[i].addEventListener('click',(e)=>{
+    e.preventDefault();//링크 안먹게
+    let orgTarget = e.target.getAttribute('href');
+
+    let tabTarget = orgTarget.replace('#','');
+
+    for(let x = 0; x < tabContent.length ; x++){
+      tabContent[x].style.display = 'none';
+    }
+
+    document.getElementById(tabTarget).style.display = "block";
+  }); 
+}
+document.getElementById('tabs1').style.display = "block";
+/* sec5 store_최성희(종료) */
