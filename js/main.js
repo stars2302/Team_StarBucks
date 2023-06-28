da275a5b50b76a8aab8a0b6deda71b13432a6d5f
stars2302 <stars2302@naver.com>
2023-06-28 16:19:27 +0900

    merge

--- js/main.js
+++ js/main.js
@@ -191,3 +191,30 @@ for(let i = 0; i <targetLink.length; i++){
 }
 document.getElementById('tabs1').style.display = "block";
 /* sec5 store_최성희(종료) */
+
+
+
+/* sec6 brand_성진영(시작) */
+let mainMenu = document.querySelectorAll('.bottom_menu > .menu_list > li'),
+    subMenu = document.querySelectorAll('.bottom_menu .menu_list li'),
+    bottom = document.querySelector('.bottom_menu'),
+    bottomHeight = bottom.offsetHeight;
+
+let subMenuHeight = 0;
+subMenu.forEach(item=>{
+  if(item.offsetHeight > subMenuHeight){
+    subMenuHeight = item.offsetHeight;
+  }
+});
+
+let bottomTotalHeight = `${bottomHeight + subMenuHeight + 30}px`;
+
+mainMenu.forEach(item=>{
+  item.addEventListener('mouseover',()=>{
+    bottom.style.height = bottomTotalHeight;
+  });
+  item.addEventListener('mouseout',()=>{
+    bottom.style.height = `${bottomHeight}px`;
+  });
+});
+/* sec6 brand_성진영(종료) */
