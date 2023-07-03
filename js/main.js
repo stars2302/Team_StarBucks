ce24429bbfb5a308c7d28cd56f27944e89707cb3
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-07-03 14:28:57 +0900

    [Add] 공지사항 slide 자동 반복 기능 추가
    무한 루프

--- js/main.js
+++ js/main.js
@@ -234,22 +234,13 @@ let sec7_container = document.querySelector('.notice_slide'), //ul
     sec7_count = sec7_slides.length, //슬라이드 갯수
     sec7_height = 60, //움직일 높이
     sec7_idx = 0;
-// console.log();
 
-//처음, 마지막 슬라이드 복제 함수
+//마지막 슬라이드 복제 함수
 function sec7_clone(){
-  let sec7_cloneFirst = sec7_slides[0].cloneNode(true);
   let sec7_cloneLast = sec7_container.lastElementChild.cloneNode(true);
-  sec7_container.append(sec7_cloneFirst);
   sec7_container.insertBefore(sec7_cloneLast, sec7_container.firstElementChild);
 } 
 sec7_clone();
-
-//슬라이드 위치값 초기화 함수
-function sec7_resetSlide(){
-  sec7_container.style.top = -(sec7_height) + 'px';
-}
-sec7_resetSlide();
 
 //복제된 슬라이드를 포함한 새로운 슬라이드 변수
 let sec7_newSlides = document.querySelectorAll(".notice_slide li");
@@ -258,16 +249,17 @@ sec7_countClone = sec7_newSlides.length;
 //공지사항 슬라이드 이동함수
 function sec7_move(num){
   sec7_container.style.top = `${-num*(sec7_height)}px`;
+  sec7_container.style.transition = `${0.5}s ease-out`;
   sec7_idx = num;
   console.log(sec7_idx);
   //마지막 슬라이드 일때
   if(sec7_idx === sec7_countClone - 1){
     //0.5초동안 복제한 첫번째에서 진짜 첫번째 위치로 이동
     setTimeout(()=>{
-      sec7_container.style.top = -(sec7_height) + 'px';
+      sec7_container.style.top = "0px";
       sec7_container.style.transition = `${0}s ease-out`;
     }, 500);
-    sec7_idx = -1;
+    sec7_idx = 0;
   }
 }
 sec7_move(0);
@@ -291,7 +283,6 @@ sec7_container.addEventListener('mouseout',()=>{
     sec7_move(nextIdx);
   },3000);
 });
-
 /* sec7 notice_천혜영(종료) */
 
 /* top button_천혜영(시작) */
